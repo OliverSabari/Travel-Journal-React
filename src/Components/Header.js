e7a4813bcfeb491726bf57e027fadd5a4b9e9cdd
Sabari <sabarihiswaran555@gmail.com>
2023-01-17 20:00:48 +0530

    header-updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,14 @@
-import { Container } from "react-bootstrap"
+
 
 const Header = () => {
     return (
-        <Container fluid>
+      
             <div className="header-div">
             <p className="header-text">
               🌍 my travel journal
             </p>
             </div>
-        </Container>
+       
     )
 }
 
