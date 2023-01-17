c19bdd4e82ddfc3e6b8c8b4076ce151631600aa9
Sabari <sabarihiswaran555@gmail.com>
2023-01-17 19:47:21 +0530

    updated-journal

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,19 +2,24 @@
 import { Container } from "react-bootstrap"
 import { TravelData } from "../Config/data"
 
+
 import TravelCards from "./TravelCards"
 
 const Body = () => {
+
+
     return (
-        <Container className="body-div">
-            {
-                TravelData.map(data => {
-                    return (
-                        <TravelCards key={data.id} {...data} />
-                    )
-                })
-            }
-        </Container>
+        <>
+            <Container className="body-div">
+                {
+                    TravelData.map(data => {
+                        return (
+                            <TravelCards key={data.id} {...data} />
+                        )
+                    })
+                }
+            </Container>
+        </>
     )
 }
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,15 @@
 import { Container } from "react-bootstrap"
 
+import ScrollToTop from "react-scroll-to-top"
+
 const Footer = () => {
     return(
+        <>
         <Container>
-            <p style={{textAlign : "center" ,color:"#F55A5A",fontSize : "13px"}}> © 2023 || Developed By Sabari. All rights reserved.</p>
+            <p style={{textAlign : "center" ,color:"#F55A5A",fontSize : "13px",fontWeight:"bold"}}> © 2023 || Developed By Sabari. All rights reserved.</p>
         </Container>
+         <ScrollToTop smooth top="100" color="#F55A5A" />
+         </>
     )
 }
 
